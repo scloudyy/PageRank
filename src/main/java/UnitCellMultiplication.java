0a3163cf74f83a41863d94e14140e17f192bc6e4
scloudyy <onecloud.shen@gmail.com>
2018-08-14 22:36:34 +0800

    feat(UnitCellMultiplication): add main

--- src/main/java/UnitCellMultiplication.java
+++ src/main/java/UnitCellMultiplication.java
@@ -1,6 +1,12 @@
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -60,5 +66,25 @@ public class UnitCellMultiplication {
                 context.write(new Text(outputKey), new Text(outputValue));
             }
         }
+    }
+
+    public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
+        Configuration configuration = new Configuration();
+        Job job = Job.getInstance(configuration);
+        job.setJarByClass(UnitCellMultiplication.class);
+
+        job.setReducerClass(MultiplicationReducer.class);
+
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(Text.class);
+
+        //import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
+        //import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+        //import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+        MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, TransitionMapper.class);
+        MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class, PRMapper.class);
+
+        FileOutputFormat.setOutputPath(job, new Path(args[2]));
+        job.waitForCompletion(true);
     }
 }
