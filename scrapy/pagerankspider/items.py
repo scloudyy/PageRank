97ca874a74c0126e45a8edcb7a2f39261f8d835e
scloudyy <onecloud.shen@gmail.com>
2018-08-17 12:19:46 +0800

    feat(scrapy): add FromToItem

--- scrapy/pagerankspider/items.py
+++ scrapy/pagerankspider/items.py
@@ -8,7 +8,6 @@
 import scrapy
 
 
-class PagerankspiderItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class FromToItem(scrapy.Item):
+    origin = scrapy.Field()
+    to = scrapy.Field()
