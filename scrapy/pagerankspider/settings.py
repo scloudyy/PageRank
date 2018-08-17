11aa11e99d5727d961f12247bf404e9cf704d8a2
scloudyy <onecloud.shen@gmail.com>
2018-08-17 12:29:15 +0800

    feat(scrapy): modify settings

--- scrapy/pagerankspider/settings.py
+++ scrapy/pagerankspider/settings.py
@@ -19,7 +19,7 @@ NEWSPIDER_MODULE = 'pagerankspider.spiders'
 #USER_AGENT = 'pagerankspider (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
@@ -64,9 +64,10 @@ ROBOTSTXT_OBEY = True
 
 # Configure item pipelines
 # See https://doc.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    'pagerankspider.pipelines.PagerankspiderPipeline': 300,
-#}
+ITEM_PIPELINES = {
+    'pagerankspider.pipelines.MatrixPipeline': 200,
+    #'pagerankspider.pipelines.JsonLinePipeline': 400,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://doc.scrapy.org/en/latest/topics/autothrottle.html
